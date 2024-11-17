5e0de1c478d954088cd7b68c15ccdaec37fd0778
Artem Tsyganov <asterionblaze@gmail.com>
2024-11-17 15:25:45 +0300

    fix: Make server actions return error object

---
+++ src/shared/api/actions/addPlaylist.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { revalidateTag } from "next/cache";
+
+import { Playlist } from "../model/playlist/playlist.types";
+
+const BASE_API_URL = process.env.BASE_API_URL ?? "http://localhost:4000";
+
+// Will be assigned by the server
+type CreatedPlaylist = Omit<Playlist, "id">;
+
+export const addPlaylistOnServerSide = async (playlist: CreatedPlaylist) => {
+    const response = await fetch(`${BASE_API_URL}/playlists/add`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(playlist)
+    });
+
+    if (!response.ok) {
+        return {
+            message: response.statusText,
+            code: response.status
+        };
+    }
+
+    revalidateTag("playlists");
+};
