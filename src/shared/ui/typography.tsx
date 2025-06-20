68c53e9cd3d6382999d3fa1aee0844410913369e
Artem Tsyganov <asterionblaze@gmail.com>
2025-06-21 02:20:23 +0300

    fix: fix typography styles

--- src/shared/ui/typography.tsx
+++ src/shared/ui/typography.tsx
@@ -14,7 +14,7 @@ export const H1 = (
   return (
     <h1
       className={
-        cn('scroll-m-20 text-center text-4xl font-extrabold'
+        cn('scroll-m-20 text-4xl font-extrabold'
         + ' tracking-tight text-balance', className)}
       {...props}
     >
@@ -28,7 +28,7 @@ export const H2 = (
   return (
     <h2
       className={
-        cn('scroll-m-20 border-b pb-2 text-3xl font-semibold'
+        cn('scroll-m-20 pb-2 text-3xl font-semibold'
         + ' tracking-tight first:mt-0', className)}
       {...props}
     >
@@ -42,7 +42,7 @@ export const H3 = (
   return (
     <h3
       className={
-        cn('scroll-m-20 text-2xl font-semibold tracking-tight', className)}
+        cn('scroll-m-20 text-xl font-semibold tracking-tight', className)}
       {...props}
     >
       {children}
@@ -55,7 +55,7 @@ export const H4 = (
   return (
     <h4
       className={
-        cn('scroll-m-20 text-xl font-semibold tracking-tight', className)}
+        cn('scroll-m-20 text-lg font-semibold tracking-tight', className)}
       {...props}
     >
       {children}
@@ -68,7 +68,7 @@ export const P = (
   return (
     <p
       className={
-        cn('leading-7 [&:not(:first-child)]:mt-6', className)}
+        cn('leading-7', className)}
       {...props}
     >
       {children}
