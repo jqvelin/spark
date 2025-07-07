15d9312abf42a14b40a24d873c1c8d890e2d94f5
Artem Tsyganov <asterionblaze@gmail.com>
2025-07-07 17:47:24 +0300

    refactor: refactor ui component classNames

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -28,7 +28,7 @@ const buttonVariants = cva(
           + ' focus-visible:ring-destructive/20'
           + ' dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
-          'border bg-background shadow-xs hover:bg-accent'
+          'border shadow-xs hover:bg-accent'
           + ' hover:text-accent-foreground dark:bg-input/30 dark:border-input'
           + ' dark:hover:bg-input/50',
         secondary:
