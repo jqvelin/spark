3d3ff906415c2b2bb32c9cb9eb2d7daeacdf003a
Artem Tsyganov <asterionblaze@gmail.com>
2025-07-07 17:48:51 +0300

    refactor: export ui components from a single index.ts file

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,6 +1,8 @@
+export { Badge } from './badge';
 export { Button } from './button';
+export * from './card';
+export { Input } from './input';
 export { Logo } from './Logo';
 export { Search } from './Search';
-export { Input } from './input';
 export * from './sheet';
 export * from './typography';
